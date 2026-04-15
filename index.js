36f81b57fc75a78b5b5415719d990060bc64f330
sirkiruthi <orencaroli@gmail.com>
2026-04-15 09:42:39 +0300

    Changes on index.js

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
 
+  for (let arr of args) {
+    combinedObject.users = [...combinedObject.users, ...arr];
+  }
+
+  combinedObject.merge_date = new Date().toString('M/d/yyyy');
+
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
